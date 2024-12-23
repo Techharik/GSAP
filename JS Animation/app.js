be0d18554bcca59888cab959d30f66e00c79dc04
Hari Prasath <khariprasath30@gmail.com>
2024-12-23 12:22:29 +0530

    to method in Gsap

---
+++ JS Animation/app.js
@@ -0,0 +1,14 @@
+// console.log(gsap) - gsap obejct
+// gsap.method(element , varsobj)
+
+
+//to - where to 
+
+gsap.to(".box", {
+    y: 100,
+    x: -100,
+    duration: 2,
+    repeat: 2
+})
+
+//set method.
