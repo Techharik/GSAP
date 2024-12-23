02e45d6660a9e99cd97b4b2b80b61de5a5998721
Hari Prasath <khariprasath30@gmail.com>
2024-12-23 13:01:37 +0530

    set methodd

--- JS Animation/app.js
+++ JS Animation/app.js
@@ -2,13 +2,28 @@
 // gsap.method(element , varsobj)
 
 
-//to - where to 
+//to - where to
 
-gsap.to(".box", {
-    y: 100,
-    x: -100,
-    duration: 2,
-    repeat: 2
-})
+// gsap.to(".box", {
+//     y: 100,
+//     x: -100,
+//     duration: 2,
+//     repeat: 2
+// })
 
 //set method.
+// gsap.set(".box", {
+//     opacity: .6,
+//     background: "red",
+// });
+
+
+// gsap.to(".box", {
+//     opacity: 1,
+//     background: "crimson",
+//     y: 200,
+//     duration: 5,
+//     ease: "bounce.out",
+//     repeat: -1,
+//     yoyo: true
+// });
